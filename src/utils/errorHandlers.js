0c6998d39e82ff96152a361b26e2d1547d3bdb38
Assane Diallo <assane@pando2.com>
2023-08-06 10:55:47 +0200

    [#2] Feat: Add error handling function that logs error and sends response

---
+++ src/utils/errorHandlers.js
@@ -0,0 +1,12 @@
+/**
+ * Handles errors that occur during the execution of a program.
+ * Logs the error stack trace and returns an HTTP response with a 500 status code and a JSON object containing an error message.
+ * @param {Error} err - The error object that was thrown.
+ * @param {Object} res - The HTTP response object that will be sent back to the client.
+ */
+function errorHandler(err, res) {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+}
+
+export default errorHandler;
