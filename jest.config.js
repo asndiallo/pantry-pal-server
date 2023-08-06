d371c96615b3f7e0061073275d880ccf6623dd08
Assane Diallo <assane@pando2.com>
2023-08-06 19:59:16 +0200

    [#6] make the `jest` object available in the global scope of all our tests

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ export default {
     moduleFileExtensions: ["js", "json", "ts", "tsx", "node"],
     testEnvironment: "node",
     testMatch: ["**/__tests__/**/*.test.js"],
+    setupFiles: ['./jest.setup.js'],
 };
