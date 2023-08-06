277529d7cff82762abcf08929c185b389ec8f6ab
Assane Diallo <assane@pando2.com>
2023-08-06 16:40:25 +0200

    fornat errorHandler

--- src/utils/errorHandlers.js
+++ src/utils/errorHandlers.js
@@ -1,7 +1,7 @@
 /**
  * Handles errors that occur during the execution of a program.
  * Logs the error stack trace and returns an HTTP response with a 500 status code and a JSON object containing an error message.
- * 
+ *
  * @param {Error} err - The error object that was thrown.
  * @param {Object} res - The HTTP response object that will be sent back to the client.
  * @returns {void}
