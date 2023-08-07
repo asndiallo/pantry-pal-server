4d33188e5f5fc6f329431513858bf57888b3a9f1
Assane Diallo <assane@pando2.com>
2023-08-07 14:23:11 +0200

    [#6] delete outdated test files

--- src/utils/__tests__/buildRecipePrompt.test.js
+++
@@ -1,5 +0,0 @@
-describe('buildRecipePrompt', () => {
-    it('should return the correct prompt', () => {
-        console.log('test');
-    });
-});
--- src/utils/__tests__/errorHandlers.test.js
+++
@@ -1,5 +0,0 @@
-describe('errorHandlers', () => {
-    it('test errorHandlers', () => {
-        console.log('test');
-    });
-});
